e1287fef6457d8ea41d3554fb492f5d0f398e314
Azure DevOps - Automation <azuredevops-automation@deloitte.com>
2021-05-31 14:08:38 +0530

    Exercise 2-5 Few people

--- lwcPractice/force-app/main/default/lwc/accountBrowser/accountBrowser.js
+++ lwcPractice/force-app/main/default/lwc/accountBrowser/accountBrowser.js
@@ -1,3 +1,20 @@
 import { LightningElement } from 'lwc';
 
-export default class AccountBrowser extends LightningElement {}
+export default class AccountBrowser extends LightningElement {
+
+    acctList = [];
+
+    constructor(){
+        super();
+        const acctNames = ['Rad', 'Stuart', 'Andres', 'Rahul', 'Amit'];
+        this.acctList = acctNames.map((acctName, index) => {
+            return {
+                'sobjectType' : 'Contact',
+                'Name' : acctName,
+                'PhotoUrl' : '/services/images/photo/003B0FakePictId',
+                'Id' : index
+            };
+        } );
+        console.log('Baji -->'+this.acctList);
+    }
+}
---
+++ lwcPractice/force-app/main/default/lwc/accountTiles/accountTiles.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from 'lwc';
+
+export default class AccountTiles extends LightningElement {
+    @api acctList = [];
+}
